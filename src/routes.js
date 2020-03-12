baccb8247fd1e74bc70e38dfb73c67f5fab7d578
lele28 <alessandracebracmoc@outlook.com>
2020-03-12 14:22:55 -0300

    primeiro commit

--- src/routes.js
+++ src/routes.js
@@ -2,31 +2,32 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import Main from './pages/Main';
-import Profile from './pages/Proile';
+import Profile from './pages/Profile';
+
 
 const Routes = createAppContainer(
-  createStackNavigator({
-    Main: {
-      screen: Main,
-      navigationOptions: {
-        title: "DevRadar"
-      },
-    },
-    Profile: {
-      screen: Profile,
-      navigationOptions: {
-        title: "Perfil no GitHub"
-      },
-    },
-  }, {
-    defaultNavigationOptions: {
-      headerTintColor: '#FFF',
-      headerBackTitleVisible: false,
-      headerStyle: {
-        backgroundColor: '#7D40E7'
-      },
-    },
-  })
+    createStackNavigator({
+        Main: {
+            screen: Main,
+            navigationOptions: {
+                title: 'DevRadar'
+            },
+        },
+        Profile: {
+            screen: Profile,
+            navigationOptions: {
+                title: 'Perfil no GitHub'
+            },
+        },
+    }, {
+        defaultNavigationOptions: {
+            headerTintColor: '#FFF',
+            headerBackTitleVisible: false,
+            headerStyle: {
+                backgroundColor: '#7d40e7'
+            },
+        },
+    })
 );
 
 export default Routes;
